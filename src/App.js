4282a92667282edf12c048263119721551c6a984
Jaclyn <jaclyndshahan@gmail.com>
2019-05-24 22:47:26 -0500

    created router folders

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { component } from 'react';
 
 import './App.css';
 
---
+++ src/store.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+
+import promiseMiddleware from 'redux-promise-middleware'
+
+
+
+export default createStore(
+  combineReducers({
+    
+  }),
+  applyMiddleware(promiseMiddleware)
+)
