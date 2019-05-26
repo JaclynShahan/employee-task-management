a2da996e8c7b2b8f3cccf8f7f85eb8f1316639bb
Jaclyn <jaclyndshahan@gmail.com>
2019-05-25 19:20:15 -0500

    created login layout

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -14,7 +14,22 @@ class Login extends Component {
 
   
   return (
-    <div>
+    <div className="wrapper">
+        <form className="loginForm">
+            <h1 className="header">WELCOME</h1>
+        <input
+        className="inputName"
+        placeholder="Username"
+        />
+        <input
+        className="inputPass"
+        placeholder="Password"
+        />
+        <button className="loginButton">Login</button>
+        <h4 className="h4"> Not a Member? <button className="signUp">Sign-Up</button>
+ </h4>
+        
+        </form>
     
     </div>
   );
