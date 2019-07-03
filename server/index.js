0ad0ea78dff574434c4e01e18e8547bd1b24ef4d
Jaclyn <jaclyndshahan@gmail.com>
2019-07-02 21:43:34 -0500

    worked on messaging section with sockets

--- server/index.js
+++ server/index.js
@@ -3,12 +3,47 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const massive = require('massive');
+const socketIo = require('socket.io');
+const index = require("../routes/index.js");
 require('dotenv').config();
 
 app = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.use(index);
 
+const server = http.createServer(app);
+
+const io = socketIo(server);
+
+let messages = [];
+
+const getMessage = async socket => {
+    try {
+        socket.emit('/api/getMessage', res.data);
+    } catch (error) {
+        console.log(`Error: ${error.code}`);
+    }
+}
+
+
+app.post('/api/newMessage', (req,res) => {
+    messages.push(req.query.message)
+    io.emit('msgs', messages)
+})
+
+let interval;
+
+io.on("connection", socket => {
+    console.log("New Client Connected");
+    if (interval) {
+        clearInterval(interval);
+    }
+    interval = setInterval(() => getMessage(socket), 3000);
+    socket.on("disconnect", () => {
+        console.log("Client Disconnected");
+    })
+})
 app.post('/api/createUser', (req, res) => {
     const dbInstance = req.app.get('db')
     dbInstance.createUser(req.body.first_name, req.body.last_name, req.body.username, req.body.password).then((resp) => {
